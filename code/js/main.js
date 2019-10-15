49c542ecd60e43b4eef13d1ed166d2e78afeb241
Hristo Manov <hmanov93@gmail.com>
2019-10-15 19:44:41 +0300

    Initial commit

---
+++ code/js/main.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Hristo on 25.3.2019 г..
+ */
+$(document).ready(function(){
+
+    $('.slider').bxSlider({
+        auto: true,
+        mode: 'vertical',
+        speed: 1000
+    });
+
+
+});
+
